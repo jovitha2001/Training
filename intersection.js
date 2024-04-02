5e27cb8262f91471e56774390613f5e77e92be87
jovitha2001 <165787492+jovitha2001@users.noreply.github.com>
2024-04-02 17:09:40 +0530

    Add files via upload

--- intersection.js
+++ intersection.js
@@ -1,11 +1,11 @@
 //Intersection of two arrays
 let nums1=[1,2,3,5,3];
+let nums2=[1,2,3,5];
 let n=nums1.length;
 let m=nums2.length;
-let nums2=[2,4,3,3];
 let nums3=[];
-for(let i=0;i<n;i++){
-    for(let j=i;j<m;j++){
+for(let i=0;i< n;i++){
+    for(let j=i;j< m;j++){
         if(nums1[i]==nums2[j]){
             nums3.push(nums1[i]);
         }
@@ -13,12 +13,5 @@ for(let i=0;i<n;i++){
 }
 console.log(nums3);
 
-/*Finding missing elements
-let nums=[3,0,1]
-nums.sort();
-for(let i=1;i<=nums.length;i++){
-    if(nums[i]!=i){
-        console.log(i);
-    }
 
-}*/
+
