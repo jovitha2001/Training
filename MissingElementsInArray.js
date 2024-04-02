13a7e2edd70a520e7325b1998a9db93567a47cff
jovitha2001 <165787492+jovitha2001@users.noreply.github.com>
2024-04-02 22:55:18 +0530

    Add files via upload

---
+++ MissingElementsInArray.js
@@ -0,0 +1,14 @@
+//Finding missing elements
+const array = [1,0, 2, 4, 3, 6, 8,10];
+const obj = {};
+for (let j = 0; j < array.length; j++) {
+    const element= array[j];
+    obj[element] = true;
+}
+let FindMissing = false;
+for (let i = array[0]; i <= array[array.length - 1]; i++) {
+    if (!obj[i]) {
+        FindMissing = true;
+        console.log(i);
+    }
+}
