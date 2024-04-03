a48277be79e2502d65e1a5101c87995bc91194be
jovitha2001 <165787492+jovitha2001@users.noreply.github.com>
2024-04-03 15:47:42 +0530

    Update MissingElementsInArray.js

--- MissingElementsInArray.js
+++ MissingElementsInArray.js
@@ -1,5 +1,7 @@
 //Finding missing elements
 const array = [1,0, 2, 4, 3, 6, 8,10];
+array.sort();
+function missingelement(array){
 const obj = {};
 for (let j = 0; j < array.length; j++) {
     const element= array[j];
@@ -9,6 +11,7 @@ let FindMissing = false;
 for (let i = array[0]; i <= array[array.length - 1]; i++) {
     if (!obj[i]) {
         FindMissing = true;
-        console.log(i);
+        return i;
     }
 }
+};
