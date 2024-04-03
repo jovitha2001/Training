22f8591099b46bb1728c24a195497ddd3a6cab08
jovitha2001 <165787492+jovitha2001@users.noreply.github.com>
2024-04-03 16:46:39 +0530

    Update IsomorphicStrings.js

--- IsomorphicStrings.js
+++ IsomorphicStrings.js
@@ -1,7 +1,4 @@
 function isIsomorphic(firstString, secondString) {
-    if (firstString.length !== secondString.length) {
-        return false;
-    }
     for (let i = 0; i < firstString.length; i++) {
         if (firstString.indexOf(firstString[i]) !== secondString.indexOf(secondString[i])) {
             return false;
